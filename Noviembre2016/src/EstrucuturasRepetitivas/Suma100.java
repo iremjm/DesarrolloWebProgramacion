3968e5bc6612294ff16d60b53fe80cbe66d47a6c
DW31 <ik012982i9@H012982.NET>
2016-10-19 13:36:23 +0200

    Ajedrez

---
+++ Noviembre2016/src/EstrucuturasRepetitivas/Suma100.java
@@ -0,0 +1,14 @@
+package EstrucuturasRepetitivas;
+
+public class Suma100 {
+
+	public static void main(String[] args) {
+	int suma=0;
+		for(int i= 1; i<=100;i++){
+			suma=suma+i;
+			System.out.println(suma);
+		}
+	
+	}
+
+}
